e0d210c790db3ce09f6d0bbe759f197455686245
MatthewTe <teelucksingh.matthew1@gmail.com>
2023-02-25 12:48:41 -0800

    Used simple maps for rendering a base maps

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Simple Maps Imports:
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const geoURL: string = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Comming Soon....Placeholder for News By Country Development
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ComposableMap>
+      <Geographies geography={geoURL}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  )}
 
 export default App;
